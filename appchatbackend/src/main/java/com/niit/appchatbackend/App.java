5bb03afce05d8fd3d34e76c2548c41213ffac8c4
Charan <Charan@Srinivas>
2019-04-04 13:30:54 +0530

    AppChat project2 backend completed successfully

---
+++ appchatbackend/src/main/java/com/niit/appchatbackend/App.java
@@ -0,0 +1,21 @@
+package com.niit.appchatbackend;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.configuration.DBConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext context=new AnnotationConfigApplicationContext(DBConfiguration.class);
+    }
+
+}
